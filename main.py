587a404e566183eb0333b5b0efe723d115527914
pavelchowdhury99 <59216690+pavelchowdhury99@users.noreply.github.com>
2022-06-28 01:06:21 +0530

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import Request, FastAPI
+from pydantic import BaseModel
+from shapely import wkb
+
+
+def convert_wkb_wkt(text: str) -> str:
+    p = wkb.loads(text, hex=True)
+    return str(p)
+
+class WKB(BaseModel):
+    wkb_hex: str
+
+app = FastAPI()
+
+@app.post("/get-wkt-from-wkb")
+async def get_email(inp: WKB, request: Request):
+    return convert_wkb_wkt(inp.wkb_hex)

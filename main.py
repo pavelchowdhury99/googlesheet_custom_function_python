3e9b96eb4d2ada30a869220118d6dbd3452570e8
pavelchowdhury99 <59216690+pavelchowdhury99@users.noreply.github.com>
2022-06-28 17:02:43 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import Request, FastAPI
 from pydantic import BaseModel
 from shapely import wkb
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
 
 
@@ -10,11 +11,14 @@ def convert_wkb_wkt(text: str) -> str:
     p = wkb.loads(text, hex=True)
     return str(p)
 
+
 class WKB(BaseModel):
     wkb_hex: str
 
+
 app = FastAPI()
 
+
 @app.post("/get-wkt-from-wkb")
-def get_email(inp: WKB, request: Request):
+def convert(inp: WKB, request: Request):
     return convert_wkb_wkt(inp.wkb_hex)

9219e9a87182fa7b455437732df1326e306d451c
pavelchowdhury99 <59216690+pavelchowdhury99@users.noreply.github.com>
2022-07-24 12:59:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,9 +17,5 @@ app = FastAPI()
 def get_wkt_from_wkb(inp:WKB_HEX):
     return convert_wkb_wkt(inp.wkb_hex)
 
-@app.get("/", include_in_schema=False)
-async def docs_redirect():
-    return RedirectResponse(url='/docs')
-
 if __name__ == '__main__':
     print(convert_wkb_wkt('010100000000b884a9329436409c878310301e5640'))
